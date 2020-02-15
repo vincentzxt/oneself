c75e534cb7e69794117a30d040b93f4fa7b2207e
vincent <vincent@vincentdeMac-mini.local>
2020-02-15 12:43:52 +0800

    update

--- jinxiaocun/unpackage/dist/dev/mp-weixin/pages/my/account/accountlist.js
+++ jinxiaocun/unpackage/dist/dev/mp-weixin/pages/my/account/accountlist.js
@@ -281,19 +281,6 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  var a0 = {
-    type: "c-product",
-    color: "#f29d6e",
-    size: 18
-  }
-  _vm.$mp.data = Object.assign(
-    {},
-    {
-      $root: {
-        a0: a0
-      }
-    }
-  )
 }
 var recyclableRender = false
 var staticRenderFns = []
@@ -348,15 +335,13 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-
-
 var _user = __webpack_require__(/*! @/api/user.js */ 286);
 var _common = __webpack_require__(/*! @/config/common.js */ 56);
-var _cuLoading = _interopRequireDefault(__webpack_require__(/*! @/components/custom/cu-loading.vue */ 247));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}var cuPanel = function cuPanel() {return __webpack_require__.e(/*! import() | components/custom/cu-panel */ "components/custom/cu-panel").then(__webpack_require__.bind(null, /*! @/components/custom/cu-panel.vue */ 604));};var cuCell = function cuCell() {return __webpack_require__.e(/*! import() | components/custom/cu-cell */ "components/custom/cu-cell").then(__webpack_require__.bind(null, /*! @/components/custom/cu-cell.vue */ 611));};var _default =
+var _cuLoading = _interopRequireDefault(__webpack_require__(/*! @/components/custom/cu-loading.vue */ 247));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}var uniList = function uniList() {return __webpack_require__.e(/*! import() | components/uni-list/uni-list */ "components/uni-list/uni-list").then(__webpack_require__.bind(null, /*! @/components/uni-list/uni-list.vue */ 618));};var uniListItem = function uniListItem() {return __webpack_require__.e(/*! import() | components/uni-list-item/uni-list-item */ "components/uni-list-item/uni-list-item").then(__webpack_require__.bind(null, /*! @/components/uni-list-item/uni-list-item.vue */ 625));};var _default =
 {
   components: {
-    cuPanel: cuPanel,
-    cuCell: cuCell },
+    uniList: uniList,
+    uniListItem: uniListItem },
 
   data: function data() {
     return {
