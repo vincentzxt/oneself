10183afb9532ac19789e07b995929ea377916ca0
xueweiwei <171720926@qq.com>
2020-02-25 15:58:43 +0800

    -wx

--- jinxiaocun/pages/bill/sell-red-list/sell-red-list.vue
+++ jinxiaocun/pages/bill/sell-red-list/sell-red-list.vue
@@ -85,7 +85,7 @@ export default {
 			orderList: [{ name: '退货日期', value: 'createtime' }, { name: '金额', value: 'amount' }]
 		};
 	},
-	onLoad() {
+	onLoad(option) {
 		if(option){
 			this.search_startDate = option.startDate
 			this.search_endDate = option.endDate
